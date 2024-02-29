b9feed30ba68a2e14eacb0c43fea80b3be96551a
Indonesian-Trying <Zidanesafaat@gmail.com>
2024-02-29 13:55:35 +0700

    Revert "web update"
    This reverts commit 5afb6cbbed7af2606473455fbc2b8263d53a165f.
    
    	modified:   routes/web.php
    	modified:   composer.lock

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\AnggotaController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\KategoriController;
 
 
 
